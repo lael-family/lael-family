ef76d0fa8a8db7a5fadd457f063db693bae92eee
David Saah <davesaah@gmail.com>
2025-06-12 16:03:01 +0000

    feat: add books section on homepage

---
+++ src/components/home/BookSection.vue
@@ -0,0 +1,64 @@
+<script lang="ts" setup>
+import Card from '../ui/card/Card.vue'
+import CardContent from '../ui/card/CardContent.vue'
+
+const books = [
+  {
+    title: 'The Power of Right Believing',
+    subtitle: '7 Keys to Freedom from Fear, Guilt, and Addiction',
+    cover: '/img/hero.jpg',
+  },
+  {
+    title: 'sample book title',
+    subtitle: 'sample book subtitle',
+    cover: '/img/hero.jpg',
+  },
+  {
+    title: 'sample book title',
+    subtitle: 'sample book subtitle',
+    cover: '/img/hero.jpg',
+  },
+  {
+    title: 'sample book title',
+    subtitle: 'sample book subtitle',
+    cover: '/img/hero.jpg',
+  },
+  {
+    title: 'sample book title',
+    subtitle: 'sample book subtitle',
+    cover: '/img/hero.jpg',
+  },
+  {
+    title: 'sample book title',
+    subtitle: 'sample book subtitle',
+    cover: '/img/hero.jpg',
+  },
+]
+</script>
+<template>
+  <div class="flex justify-between items-center">
+    <div>
+      <h2 class="font-semibold text-xl">Books</h2>
+      <p class="text-lg">Find in-depth materials on specific topics on your journey of faith</p>
+    </div>
+    <RouterLink to="/" class="text-xl font-semibold underline" style="color: var(--dark-turquoise)">
+      See more
+    </RouterLink>
+  </div>
+  <div class="grid grid-cols-6 py-4 gap-4">
+    <Card
+      v-for="book in books"
+      :key="book.title"
+      :class="[
+        `bg-[url(${book.cover})]`,
+        'transition delay-100 duration-300 ease-in-out hover:shadow-2xl cursor-pointer',
+        'bg-cover h-96 w-64',
+      ]"
+    >
+      <CardContent class="pt-64 px-2">
+        <p class="font-semibold">{{ book.title }}</p>
+        <p class="text-sm">{{ book.subtitle }}</p>
+      </CardContent>
+    </Card>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import BookSection from '@/components/home/BookSection.vue'
 import FaithAcademySection from '@/components/home/FaithAcademySection.vue'
 import HeroSection from '@/components/home/HeroSection.vue'
 import PrayerBoardSection from '@/components/home/PrayerBoardSection.vue'
@@ -6,8 +7,9 @@ import PrayerBoardSection from '@/components/home/PrayerBoardSection.vue'
 
 <template>
   <HeroSection />
-  <div class="p-7">
+  <div class="p-7 flex flex-col gap-y-5">
     <PrayerBoardSection />
+    <BookSection />
   </div>
   <FaithAcademySection />
 </template>
